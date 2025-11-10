64b85ea7bb7e751b5916fa7cb01d8d7df62ee898
maks <maks-zych@wp.pl>
2025-11-10 16:36:46 +0100

    blinking

---
+++ blinking_led.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+# Use BCM GPIO numbering
+GPIO.setmode(GPIO.BCM)
+
+# Set GPIO pin 17 as output
+LED_PIN = 17
+GPIO.setup(LED_PIN, GPIO.OUT)
+i = 0 
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)  # Turn LED on
+        time.sleep(1)                     # Wait for 1 second
+        GPIO.output(LED_PIN, GPIO.LOW)   # Turn LED off
+        time.sleep(1)                     # Wait for 1 second
+        ans = input("Czy dalej?: \nzakończyc: no")
+        if ans == "no":
+            break
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()  # Reset GPIO settings
