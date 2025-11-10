fd2cd06c9b7d7aca13ce6c3f3d71b21d7878f988
maks <maks-zych@wp.pl>
2025-11-10 16:38:31 +0100

    blinking2

--- blinking_led.py
+++ blinking_led.py
@@ -10,10 +10,11 @@ GPIO.setup(LED_PIN, GPIO.OUT)
 i = 0 
 try:
     while True:
-        GPIO.output(LED_PIN, GPIO.HIGH)  # Turn LED on
-        time.sleep(1)                     # Wait for 1 second
-        GPIO.output(LED_PIN, GPIO.LOW)   # Turn LED off
-        time.sleep(1)                     # Wait for 1 second
+        for i in range(10):
+            GPIO.output(LED_PIN, GPIO.HIGH)  # Turn LED on
+            time.sleep(1)                     # Wait for 1 second
+            GPIO.output(LED_PIN, GPIO.LOW)   # Turn LED off
+            time.sleep(1)                     # Wait for 1 second
         ans = input("Czy dalej?: \nzakończyc: no")
         if ans == "no":
             break
